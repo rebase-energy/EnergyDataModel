038e87361bd0c8f1153b0b590ce3ff451d8a5b06
sebaheg <sebastian@rebase.energy>
2025-03-22 13:38:44 +0100

    Added requirements file

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("energydatamodel/requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='energydatamodel',
     version='0.0.2',
     packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "shapely",
-        "pytz",
-        "ipywidgets",
-        "geopandas",
-        "anytree",
-        "pvlib",
-    ],
+    install_requires=requirements,
     author='rebase.energy',
     description='Data model for energy modelling.',
 )
